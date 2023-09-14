980153fa3b07367a5e4dcf0ed5f0a220f792cd42
Nils-Olof Bengtsson <nils-olof.bengtsson@cgi.com>
2023-09-14 10:12:17 +0200

    Bikeshop exercise from book Learn WPF MVVM-XAML

---
+++ Contact.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bikeshop_exercise
+{
+    /// <summary>
+    /// Interaction logic for Contact.xaml
+    /// </summary>
+    public partial class Contact : Page
+    {
+        public Contact()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
